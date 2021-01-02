4e46f8e954ddd698d20c405305edd1072dbee14e
Shawn Boyette <shawn@firepear.net>
2021-01-01 19:17:45 -0700

    reinit

---
+++ internal/data/main.go
@@ -0,0 +1,9 @@
+package data
+
+// The CPUdata struct is used by internal/hwmon to report the data
+// collected on a machine's CPU. Name is the CPU name as reported by
+// the OS, and Cores is a map of core ids to speeds in MHz.
+type CPUdata struct {
+	Name string
+	Cores map[string]string
+}
