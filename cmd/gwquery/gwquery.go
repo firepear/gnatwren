fcb2c7a53dda4b1946e79c4a4ef1b6312b7f3373
Shawn Boyette <shawn@firepear.net>
2021-03-07 15:33:11 -0700

    formatting changes

--- cmd/gwquery/gwquery.go
+++ cmd/gwquery/gwquery.go
@@ -54,7 +54,9 @@ func main() {
 
 	for _, hostname := range hosts  {
 		hostdata := metrics[hostname]
-		fmt.Printf("%s  ||  %s (%d threads)\n", hostname, hostdata.Cpu.Name, len(hostdata.Cpu.Cores))
+		fmt.Printf("%s  %s (%d threads)\n", hostname, hostdata.Cpu.Name, len(hostdata.Cpu.Cores))
+		fmt.Println("  ----------------------------------------------------------------------")
+
 		mincore, maxcore, avgcore, coretot := 0, 0, 0, 0
 		for _, core := range hostdata.Cpu.Cores {
 			clock_f, _ := strconv.ParseFloat(core, 64)
@@ -78,9 +80,10 @@ func main() {
 		s := uptime - m * 60
 		ts := time.Unix(hostdata.TS, 0).Format("2 Jan 15:04:05")
 
-		fmt.Printf("  Up %dd %02d:%02d:%02d  ||  Ldavg %s %s %s  ||  %s\n", int(d), int(h), int(m), int(s),
-			hostdata.Ldavg[0], hostdata.Ldavg[1], hostdata.Ldavg[2], ts)
-		fmt.Printf("  Min/max/avg %d / %d / %d MHz  ||  Temp %05.2fC\n", mincore, maxcore, avgcore, hostdata.Cpu.Temp)
+		fmt.Printf("  Rep %s  |  Up %dd %02d:%02d:%02d  |  Ldavg %s\n", ts, int(d), int(h), int(m), int(s), hostdata.Ldavg[2])
+		fmt.Println("  ----------------------------------------------------------------------")
+		fmt.Printf("  Min/max/avg %d / %d / %d MHz  |  Temp %05.2fC\n", mincore, maxcore, avgcore, hostdata.Cpu.Temp)
+		fmt.Println("  ----------------------------------------------------------------------")
 		fmt.Printf("  Mem tot/free/avail %05.2fGB / %05.2f%% / %05.2f%%\n\n",
 			float64(hostdata.Mem[0]) / 1024.0 / 1024.0,
 			(float64(hostdata.Mem[1]) / float64(hostdata.Mem[0]) * 100),
