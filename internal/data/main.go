040fd4a74554d71848b4f2f158640da2725bf7fe
Shawn Boyette <shawn@firepear.net>
2021-11-27 13:28:51 -0700

    removing gwquery

--- internal/data/main.go
+++ internal/data/main.go
@@ -34,19 +34,6 @@ type GatherFileConfig struct {
 }
 
 
-// Query represents a request for information from gwquery to
-// gwgather. Op is the query operation to be performed. Hosts is a
-// list of hostnames for limiting by host. Tbegin and Tend are
-// timestamps delineating the timespan that data is being requested
-// for, with Tend meaning "now" when not specified/null
-type Query struct {
-	Op string      `json:"op"`
-	Hosts []string `json:"hosts"`
-	Tbegin int64   `json:"time_begin"`
-	Tend int64     `json:"time_end"`
-}
-
-
 // AgentStatus is a repackaged AgentPayload that adds the most recent
 // check-in time from gwgather
 type AgentStatus struct {
