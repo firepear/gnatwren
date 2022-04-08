e768819587d0ebf0a79d3859b14c8130cd2aa773
Shawn Boyette <shawn@firepear.net>
2022-04-07 22:55:39 -0600

    run gwdump via cron

--- assets/dockerstart.sh
+++ assets/dockerstart.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+# make our cron dir if it doesn't exist
 mkdir -p /etc/periodic/2min
+# dump our script to be run and chmod it
 cat > /etc/periodic/2min/gwdump <<EOF
 #!/bin/sh
 /usr/local/bin/gwdump -config /etc/gwgather-config.json
 EOF
 chmod +x /etc/periodic/2min/gwdump
+# add the dir to the system crontab
 echo "*/2 * * * *       run-parts /etc/periodic/2min" >> /etc/crontabs/root
+# turn on the cron service
+crond &
+# background nginx
 nginx -g 'daemon off;' &
+# start gwgather
 /usr/local/bin/gwgather -config /etc/gwgather-config.json
