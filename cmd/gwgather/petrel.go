0e0e8e06b815f4c8c8a2a7b8386e3193cbe4aab7
Shawn Boyette <shawn@firepear.net>
2025-08-25 12:12:54 -0600

    updates for petrel v0.40

--- cmd/gwgather/petrel.go
+++ cmd/gwgather/petrel.go
@@ -8,12 +8,13 @@ import (
 // requests
 var fresp []byte
 
-func agentUpdate(args []byte) ([]byte, error) {
+func agentUpdate(args []byte) (uint16, []byte, error) {
 	// send data to the DB. we're in blob mode, so all data will
 	// be in the zeroth byteslice
 	err := dbUpdate(args)
 	if err != nil {
 		log.Printf("agentUpdate: db err: %s", err)
+		return 500, fresp, err
 	}
-	return fresp, err
+	return 200, fresp, err
 }
